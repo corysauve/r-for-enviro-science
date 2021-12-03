246951d34b14b8e4d370dca8855aec05bcc62d06
Cory Sauve <coryjsauve@gmail.com>
2021-12-03 21:38:01 +0000

    update lake data

--- data-raw/university_lake.R
+++ data-raw/university_lake.R
@@ -5,7 +5,7 @@ plankton_clean <- plankton_raw %>%
   dplyr::slice(1:4, 6:10) %>%
   dplyr::select(-lake, -sample_type)
 
-lake_profile <- water_chem_raw %>%
+university_lake <- water_chem_raw %>%
   dplyr::slice(1:3, 5, 7:10, 12) %>%
   dplyr::select(-sample_type) %>%
   dplyr::left_join(plankton_clean, by = "depth") %>%
@@ -17,4 +17,4 @@ lake_profile <- water_chem_raw %>%
                 orgn_mgl = organic_n(tn_mgl, no3_mgl, nh3_mgl),
                 light_level_per = light_percentage(light_dep_mmol, light_sur_mmol))
 
-usethis::use_data(lake_profile, overwrite = TRUE)
+usethis::use_data(university_lake, overwrite = TRUE)
